a325f4ad33d7082775fd440d47cecfdfc8c19264
Floridado <waeldghim7@gmail.com>
2022-09-27 04:42:08 +0100

    auth initial

---
+++ controllers/AuthController.js
@@ -0,0 +1,25 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const register = (req, res, next) => {
+  const { name, email, password } = req.body;
+  bcrypt.hash(password, 10, (err, hash) => {
+    if (err) {
+      res.json({ err });
+    }
+    let newUser = new User({
+      name,
+      email,
+      password: hash,
+    });
+    newUser
+      .save()
+      .then(() => {
+        res.json({ message: "User added successfully" });
+      })
+      .catch((err) => {
+        res.json({ message: "An Error Occured!", err });
+      });
+  });
+};
