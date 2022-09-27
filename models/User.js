7bdeb66bd1a9cbef516a80cd8bf87ad9495bf450
Floridado <waeldghim7@gmail.com>
2022-09-27 04:00:44 +0100

    quick fix

--- models/User.js
+++ models/User.js
@@ -1,14 +1,17 @@
 const Mongoose = require("mongoose");
 
-const userSchema = Mongoose.Schema({
-  name: String,
-  password: String,
-  todos: {
-    task: String,
-    dateAdded: Date,
-    dueDate: Date,
+const userSchema = Mongoose.Schema(
+  {
+    name: String,
+    password: String,
+    todos: {
+      task: String,
+      dateAdded: Date,
+      dueDate: Date,
+    },
+    token: String,
   },
-  token: String,
-});
+  { timestamps: true }
+);
 
 export default Mongoose.model("User", userSchema);
