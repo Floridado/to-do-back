2d98e559f3891ff271519b34e7d9cbb2fe8b9183
Floridado <waeldghim7@gmail.com>
2022-09-28 17:17:58 +0100

    update

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,8 +1,16 @@
+const {
+  pwChange,
+  emailChange,
+  nameChange,
+} = require("../controllers/credsController");
 const userData = require("../controllers/profileController");
 const auth = require("../middleware/authenticate");
 
 const router = require("express").Router();
 
-router.post("/", auth, userData);
+router.get("/", auth, userData);
+router.put("/pw", auth, pwChange);
+router.put("/mail", auth, emailChange);
+router.put("/name", auth, nameChange);
 
 module.exports = { dash: router };
