6ae4c4a458374809bff3064b4ed876cd87aa992a
Floridado <waeldghim7@gmail.com>
2022-09-28 17:10:16 +0100

    creds change

--- controllers/credsController.js
+++ controllers/credsController.js
@@ -1,10 +1,27 @@
 const User = require("../models/User");
 
 const nameChange = (req, res) => {
-  const { name, id: _id } = req.body;
-  User.findById(id).then((user) => {
-    User.updateOne({ id, $set: { name } }).catch((error) =>
-      res.json({ error })
-    );
-  });
+  const { name } = req.user;
+  const { username, password } = req.body;
+  User.updateOne({ name }, { $set: { name: username } })
+    .then(() => res.json({ message: "updated" }))
+    .catch((error) => res.json({ err: error.message }));
 };
+
+const emailChange = (req, res) => {
+  const { name } = req.user;
+  const { email, password } = req.body;
+  User.updateOne({ name }, { $set: { email } })
+    .then(() => res.json({ message: "updated" }))
+    .catch((error) => res.json({ err: error.message }));
+};
+
+const pwChange = (req, res) => {
+  const { name } = req.user;
+  const { password } = req.body;
+  User.updateOne({ name }, { $set: { password } })
+    .then(() => res.json({ message: "updated" }))
+    .catch((error) => res.json({ err: error.message }));
+};
+
+module.exports = { nameChange, emailChange, pwChange };
